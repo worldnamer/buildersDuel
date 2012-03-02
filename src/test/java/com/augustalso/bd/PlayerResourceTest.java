eb67273651f3613b3bdde38141d98d0fcd07cf99
John LeBoeuf-Little <worldnamer@worldnamer.com>
2012-03-01 23:09:46 -0700

    Organizing imports.

--- src/test/java/com/augustalso/bd/PlayerResourceTest.java
+++ src/test/java/com/augustalso/bd/PlayerResourceTest.java
@@ -2,9 +2,7 @@ package com.augustalso.bd;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
