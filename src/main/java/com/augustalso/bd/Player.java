7a7fdf415a346be126dad3e4549d15e6c8308343
Austin Bookheimer <Austin.bookheimer@gmail.com>
2012-03-01 22:16:27 -0700

    Parameterized Player with ResourceFactory, added Reserve and Supply constructs

--- src/main/java/com/augustalso/bd/Player.java
+++ src/main/java/com/augustalso/bd/Player.java
@@ -1,9 +1,36 @@
 package com.augustalso.bd;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Player {
 	
+	private ResourceFactory factory;
+	private Set<Resource> supply = new HashSet<Resource>();
+	private Set<Resource> reserve = new HashSet<Resource>();
+	
 	private int gold = 3;
-
+	
+	public Player(){
+		
+	}
+	
+	public Player(ResourceFactory factory){
+		this.factory = factory;
+		
+		supply.add(factory.getWoodResource1());
+		supply.add(factory.getRockResource1());
+		supply.add(factory.getSandResource1());
+		
+		List<Resource> resources = Arrays.asList(factory.getWoodResource2(), factory.getSandResource2(), factory.getRockResource2(),
+                factory.getSheepResource1(), factory.getSheepResource2(), factory.getSheepResource3(), factory.getSheepResource4(),
+                factory.getOreResource1());
+	
+		reserve.addAll(resources);
+	}
+	
 	public int getGold() {
 		return this.gold;
 	}
@@ -21,5 +48,34 @@ public class Player {
 		}
 	}
 
+	public Object getResourceFactory() {
+		
+		return factory;
+	}
+
+	public void setResourceFactory(ResourceFactory factory) {
+		this.factory = factory;
+		
+	}
+
+	public Set<Resource> getSupply() {
+		return supply;
+	}
+
+	public Set<Resource> getReserve() {
+		return reserve;
+	}
+
+	public void toSupply(Resource resource) {
+		reserve.remove(resource);
+		supply.add(resource);
+	}
+
+	public void toReserve(Resource resource) {
+		supply.remove(resource);
+		reserve.add(resource);
+	}
+
+	
 	
 }
--- src/test/java/com/augustalso/bd/PlayerResourceTest.java
+++ src/test/java/com/augustalso/bd/PlayerResourceTest.java
@@ -1,9 +1,74 @@
 package com.augustalso.bd;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerResourceTest {
 	
+	private Player player;
+	private ResourceFactory factory;
+	
+	@Before
+	public void playermaker(){
+		factory = new ResourceFactory();
+		player = new Player(factory);
+	}
+	
+	@Test
+	public void canSetResourceFactory() {
+		assertEquals(factory, player.getResourceFactory());
+	}
+	
+	@Test
+	public void canHaveSupply() {
+		Set<Resource> supply = player.getSupply();
+		assertNotNull(supply);
+	}
+	
+	@Test
+	public void supplyHasThreeResources(){
+		List<Resource> checkfor = Arrays.asList(factory.getWoodResource1(), factory.getSandResource1(), factory.getRockResource1());
 
+		Set<Resource> supply = player.getSupply();
+		
+		assertEquals(checkfor.size(), supply.size());
+		assertTrue(supply.containsAll(checkfor));
+	}
+	
+	@Test
+	public void canHaveReserve(){
+		List<Resource> checkfor = Arrays.asList(factory.getWoodResource2(), factory.getSandResource2(), factory.getRockResource2(),
+                factory.getSheepResource1(), factory.getSheepResource2(), factory.getSheepResource3(), factory.getSheepResource4(),
+                factory.getOreResource1());
+
+		Set<Resource> supply = player.getReserve();
+		
+		assertEquals(checkfor.size(), supply.size());
+		assertTrue(supply.containsAll(checkfor));
+	}
+	
+	@Test
+	public void canMoveToSupply(){
+		player.toSupply(factory.getSheepResource1());
+		
+		assertTrue(player.getSupply().contains(factory.getSheepResource1()));
+		assertFalse(player.getReserve().contains(factory.getSheepResource1()));
+	}
+	
+	@Test
+	public void canMoveToReserve(){
+		player.toReserve(factory.getWoodResource1());
+		
+		assertTrue(player.getReserve().contains(factory.getWoodResource1()));
+		assertFalse(player.getSupply().contains(factory.getWoodResource1()));
+	}
 
 }
